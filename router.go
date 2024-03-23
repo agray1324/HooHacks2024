8d500c37b48fdc0e02dba84570e92d241ff23016
agray1324 <85634946+agray1324@users.noreply.github.com>
2024-03-23 13:00:57 -0400

    server

---
+++ router.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func Router() *gin.Engine {
+    r := gin.Default()
+    r.GET("/", func(c *gin.Context) {
+        c.Header("Content-Type", "text/html")
+        c.HTML(http.StatusOK, "index.html", gin.H{})
+    })
+
+    return r
+}
--- server.go
+++ server.go
@@ -1,15 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
 
-func main(){
-	server := gin.Default()
 
-	server.GET("/test", func(ctx *gin.Context){
-		ctx.JSON(200,gin.H{
-			"message": "ok",
-		})
-	})
-
-	server.Run(":8080")
+func Server(){
+    r := router.Router()    
+    r.Run(":8080")  
+    
 }
