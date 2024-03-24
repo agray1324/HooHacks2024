de4b2aeeaaf1ab108223fe4b903bd1ad1fbe94f2
agray1324 <85634946+agray1324@users.noreply.github.com>
2024-03-23 20:13:52 -0400

    styling

--- server/resources/js/search_control.js
+++ server/resources/js/search_control.js
@@ -6,7 +6,10 @@ window.onload = () => {
     list = ""
     console.log(_urls.length)
     for (let i = 0; i < _urls.length; i++){
-        list += "<div class=\"listrow row\"><pre><h1 id=\"title_" + String(i) + "\">" + _titles[i] + "  <a href=\"" + _urls[i] + "\"><i class=\"fa-solid fa-right-to-bracket\"></i></a></h1><pre><p1 class=\"col-12\" id=\"data_" + String(i) + "\">" + _data[i] + "</p1></div><br>"
+        /*if(list != ""){
+            list += "<br>"
+        }*/
+        list += "<div class=\"listrow row\"><pre><h1 id=\"title_" + String(i) + "\">" + _titles[i] + "  <a href=\"" + _urls[i] + "\"><i class=\"fa-solid fa-right-to-bracket\"></i></a></h1><pre><p1 class=\"col-12\" id=\"data_" + String(i) + "\">" + _data[i] + "</p1></div>"
     } 
     document.getElementById("list").innerHTML = list
 }
